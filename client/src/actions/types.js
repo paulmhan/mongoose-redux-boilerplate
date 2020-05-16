0606fd1877d50144088937576e21c07f1070417f
Paul Han <paul.myung.han@gmail.com>
2020-05-16 11:32:20 -0700

    created todosReducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,8 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
+
+
+
+//todos section
+export const GET_ALL_TODOS = "GET_ALL_TODOS";
+export const GET_ALL_TODOS_ERROR = "GET_ALL_TODOS_ERROR";
